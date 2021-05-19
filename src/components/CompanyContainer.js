9b09c829bc463b482b1ee30d81cfd5b00ba54455
brugobi <brgobbi@hotmail.com>
2021-05-19 09:46:00 -0400

    replace a nest ternary expression for switch

--- src/components/CompanyContainer.js
+++ src/components/CompanyContainer.js
@@ -16,52 +16,59 @@ const CompanyContainer = () => {
   const loading = useSelector((state) => state.stock.loading);
   const errorMsg = useSelector((state) => state.stock.error);
 
-  return loading ? (
-    <h2>Loading</h2>
-  ) : errorMsg ? (
-    <h2>{errorMsg}</h2>
-  ) : (
-    <div>
-      <div className="container">
-        <div className="card-wrapper">
-          <div className="card stock-card">
-            <div className="content">
-              <div>
-                <h1>
-                  {stockData.name}
-                  {' '}
-                  -
-                  {' '}
-                  {stockData.symbol}
-                </h1>
-                <p>
-                  Price - $
-                  {stockData.price}
-                </p>
-                <p>
-                  dayLow - $
-                  {stockData.dayLow}
-                </p>
-                <p>
-                  dayHigh - $
-                  {stockData.dayHigh}
-                </p>
-                <p>
-                  exchange -
-                  {stockData.exchange}
-                </p>
+  switch (true) {
+    case loading:
+      return (
+        <h2>Loading</h2>
+      );
+    case errorMsg:
+      return (
+        <h2>{errorMsg}</h2>
+      );
+    default:
+      return (
+        <div>
+          <div className="container">
+            <div className="card-wrapper">
+              <div className="card stock-card">
+                <div className="content">
+                  <div>
+                    <h1>
+                      {stockData.name}
+                      {' '}
+                      -
+                      {' '}
+                      {stockData.symbol}
+                    </h1>
+                    <p>
+                      Price - $
+                      {stockData.price}
+                    </p>
+                    <p>
+                      dayLow - $
+                      {stockData.dayLow}
+                    </p>
+                    <p>
+                      dayHigh - $
+                      {stockData.dayHigh}
+                    </p>
+                    <p>
+                      exchange -
+                      {stockData.exchange}
+                    </p>
+                  </div>
+                </div>
+                <div className="card-footer">
+                  <button type="button" className="button is-danger is-light btn-more card-footer-item">
+                    <Link to="/">Close</Link>
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="card-footer">
-              <button type="button" className="button is-danger is-light btn-more card-footer-item">
-                <Link to="/">Close</Link>
-              </button>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+  }
 };
 
 export default CompanyContainer;
